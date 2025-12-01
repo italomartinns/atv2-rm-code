8ed460db5a4624208bf4cd561bfc95328b350b41
italo <italomartinns@outlook.com>
2025-12-01 02:05:58 -0300

    teste 123

--- ClientLauncher.py
+++ ClientLauncher.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 		serverPort = sys.argv[2]
 		rtpPort = sys.argv[3]
 		fileName = sys.argv[4]	
+		# teste 1 2 3
 	except:
 		print("[Usage: ClientLauncher.py Server_name Server_port RTP_port Video_file]\n")	
 		sys.exit(1)
